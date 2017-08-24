799643e93f62ea8ad2f90b4e9db059e2fa1c47e4
Santosh <santosharakere@gmail.com>
2017-08-24 08:09:45 +0530

    signup form

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -16,6 +16,20 @@ const MyFirstComponent = function () {
       <MyTitle title="Game of Thromes" color="YellowGreen" />
       <MyTitle title="The Chase" color="GreenYellow" />
       <MyTitle title="Grand Design" color="burlywood" />
+      <SignUpForm/>
+    </div>
+  )
+}
+
+const SignUpForm = function () {
+  return (
+    <div>
+      <form action="/signup" method="post">
+        <div>
+          <label for="name">Name: </label>
+          <input type="text"></input>
+        </div>
+      </form>
     </div>
   )
 }
