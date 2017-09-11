3cb3984cec87294828185bd7cf3b5776421faeed
Santosh <santosharakere@gmail.com>
2017-09-11 20:56:49 +0100

    fixed eslint issues. Now using airbnb eslint rules

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,24 +1,11 @@
-import React from 'react'
-import { render } from 'react-dom'
-// const ce = React.createElement
-const MyTitle = function (props) {
-  return (
-    <div>
-      <h1 style={ { color: props.color } }>{props.title}</h1>
-    </div>
-  )
-}
+import React from 'react';
+import { render } from 'react-dom';
 
-const MyFirstComponent = function () {
-  return (
-    <div id="my-first-component">
-      <input type="text"/>
-      <MyTitle title="Game of Thromes" color="YellowGreen" />
-      <MyTitle title="The Chase" color="GreenYellow" />
-      <MyTitle title="Grand Design" color="burlywood" />
-      <SignUpForm/>
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>svideo</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
     </div>
-  )
-}
-
-render(<MyFirstComponent />, document.getElementById('app'))
+  </div>);
