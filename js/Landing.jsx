cc334e3cbca79db5d388d4fea9e9fe4e753b73ca
Santosh <santosharakere@gmail.com>
2017-09-13 21:55:32 +0100

    landing and search routes

---
+++ js/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Landing = () => (
+  <div className="landing">
+    <h1>svideo</h1>
+    <input type="text" placeholder="search" />
+    <a>or Browse All</a>
+  </div>
+);
+
+export default Landing;
---
+++ js/Search.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Search = () => (
+  <h1>hi lol this is search</h1>
+);
+
+export default Search;
