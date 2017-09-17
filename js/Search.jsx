c545ef9ae69ef7c9ce4634edc5e4620f217bf07a
Santosh <santosharakere@gmail.com>
2017-09-17 07:21:07 +0100

    showcard inside search

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,7 +4,16 @@ import preload from '../data.json';
 
 const Search = () => (
   <div className="search">
-    { preload.shows.map(show => <h3>{show.title}</h3>)}
+    { preload.shows.map(show => (
+      <div className="show-card">
+        <img alt={`${show.title} Show Poster`} src={`/public/img/${show.poster}`} />
+        <div>
+          <h3>{show.title}</h3>
+          <h4>({show.year})</h4>
+          <p>{show.description}</p>
+        </div>
+      </div>
+    ))}
   </div>
 );
 
