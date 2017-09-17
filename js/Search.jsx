47a6188432cd132ddb0298f337cc8a119a06cf1b
Santosh <santosharakere@gmail.com>
2017-09-17 06:59:06 +0100

    json data complete

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,9 +4,7 @@ import preload from '../data.json';
 
 const Search = () => (
   <div className="search">
-    <pre>
-      <code>{JSON.stringify(preload, null, 4)}</code>
-    </pre>
+    { preload.shows.map(show => <h3>{show.title}</h3>)}
   </div>
 );
 
