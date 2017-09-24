0cdc5563aed836fe0333ea03f4faa2cc034973ca
Santosh <santosharakere@gmail.com>
2017-09-24 19:11:57 +0100

    css for search component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+`;
 
 const Search = () => (
-  <div className="search">
+  <Wrapper className="search">
     { preload.shows.map(show => (
       <ShowCard key={show.imdbID} show={show} />
     ))}
-  </div>
+  </Wrapper>
 );
 
 export default Search;
